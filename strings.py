5acae1238ec13e3af6c0bbad09cf2050a6b2f1a8
hillockmakori97-hue <hillockmakori97@gmail.com>
2026-04-13 16:29:08 +0300

    first commit

---
+++ strings.py
@@ -0,0 +1,17 @@
+first_name="hillock"
+last_name="mobunde"
+print(type(first_name))
+num=100
+num1=100.1
+value=True
+print((num))
+print((num1))
+print((value))
+full_name=first_name+" "+last_name
+name="my name is ropy kevin"
+print(len(name))
+print(name[14])
+print(name[-7])
+text2="i am a software student and i am learning python programming"
+print(text2[42:61])
+print(text2[0:24])
---
+++ variables.py
@@ -0,0 +1,13 @@
+my_number="0724668547"
+first_name="Hilock"
+num1=100
+num2=100
+total=num1+num2
+print(total)
+print(first_name)
+#  write a program tah calculates tha area of a square
+length=20
+width=length
+area=length*width
+print(area)
+
