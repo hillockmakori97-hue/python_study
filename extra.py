9568e2135b5a86c4d7e754b6054510167c6a85ed
hillockmakori97-hue <hillockmakori97@gmail.com>
2026-04-14 16:43:31 +0300

    string methods

---
+++ extra.py
@@ -0,0 +1,22 @@
+my_name=input("enter your name: ")
+print(my_name)
+
+#get 2 numbers from input amd sum them 
+num1=input("enter the first number;")
+num2=input("enter the second number;")
+num1=int(num1)
+num2=(int(num2))
+total=num1+num2
+print(total)
+
+number=1000
+print(type(number))
+number=str(number)
+print(type(number))
+number=float(number)
+print(type(number))
+print(number)
+
+number1=95.12345
+number2=round(number1,4)
+print(number2)
---
+++ stringtask.py
@@ -0,0 +1,10 @@
+name="JOHn"
+name=name.lower()
+print(name)
+sentence_one="The Dog Breed Is German Sherpherd"
+print(sentence_one[8:34])
+#Defeats for the Clinton forces, this was her moment of triumph
+sentence_two="Defeats for the Clinton forces, this was her moment of triumph"
+# sentence_two=sentence_two.find("C")
+
+print(sentence_two[16:30])
