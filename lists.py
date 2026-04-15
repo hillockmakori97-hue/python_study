bc11eec893519da106f51da554fc8eaf88856d23
hillockmakori97-hue <hillockmakori97@gmail.com>
2026-04-15 16:28:39 +0300

    lists.py

---
+++ lists.py
@@ -0,0 +1,10 @@
+# Create a list of fruits
+fruits=['mango','banana','orange','lemon']
+print(type(fruits))
+print(fruits)
+print(fruits[1])
+#Update Item
+fruits[1]='apple'
+print(fruits)
+#slice
+print(fruits[2:4])
